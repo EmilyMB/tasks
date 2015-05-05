6ba9f49b0029a401741fc41dec6bcb7e9b6b0257
Emily Berkeley <eberkeley@gmail.com>
2015-05-05 09:29:36 -0600

    Add task validations

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should belong_to(:list) }
+    it { should validate_presence_of(:title) }
+  end
 end
